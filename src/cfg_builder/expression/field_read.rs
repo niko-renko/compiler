158fbc7446c530e9226c13cb63a3d6f2b9834829
NorwayFish <nikolay.university@gmail.com>
2024-03-06 17:22:03 -0500

    optimized field lookup

--- src/cfg_builder/expression/field_read.rs
+++ src/cfg_builder/expression/field_read.rs
@@ -9,13 +9,12 @@ impl Build for FieldRead {
     ) -> Result<Place, String> {
         let object = self.get_object().update(cfg, classes, function)?;
 
+        let class_name = function.get_class_name().unwrap();
+        let fields = classes.get_class_field_ids(class_name);
         let field_id = classes.get_field_id(self.get_field().get_name()).unwrap();
-        let field_map = cfg.add(Get::from(object.into(), Value::from(1).into()).into());
-        let field_offset =
-            cfg.add(Get::from(field_map.into(), Value::from(field_id).into()).into());
-        cfg.fail_if(field_offset.clone(), false, FailReason::NoSuchField);
+        let field_index = fields.iter().position(|&x| x == field_id).unwrap();
 
-        let field_value = cfg.add(Get::from(object.into(), field_offset.into()).into());
+        let field_value = cfg.add(Get::from(object.into(), Value::from(field_index).into()).into());
         Ok(field_value)
     }
 }
