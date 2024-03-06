b0d2ce9628dd6fe1e5e2b25566d8d16cc10fb430
NorwayFish <nikolay.university@gmail.com>
2024-03-05 19:21:31 -0500

    new impl

--- src/ast_extract/functions/mod.rs
+++ src/ast_extract/functions/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::*;
 
 mod function_context;
@@ -6,30 +8,39 @@ pub use function_context::FunctionContext;
 
 pub struct FunctionsContext;
 
-pub struct Functions<'ast>(Vec<FunctionContext<'ast>>);
+pub struct Functions<'ast> {
+    contexts: Vec<FunctionContext<'ast>>,
+    classes: HashSet<&'ast Name>,
+}
 
 impl Functions<'_> {
     pub fn iter(&self) -> impl Iterator<Item = &FunctionContext> {
-        self.0.iter()
+        self.contexts.iter()
     }
 
     pub fn get_method(&self, class: &Name, method: &Name) -> Option<&FunctionContext> {
-        self.0
+        self.contexts
             .iter()
             .find(|f| f.get_class_name() == Some(class) && f.get_function_name() == method)
+    }
+
+    pub fn class_exists(&self, name: &Name) -> bool {
+        self.classes.contains(name)
     }
 }
 
 impl<'ast> Extract<'ast, AST, FunctionsContext> for Functions<'ast> {
     fn extract(ast: &'ast AST, _: Option<FunctionsContext>) -> Result<Self, String> {
         let mut contexts = vec![FunctionContext::from(None, ast.get_main())];
+        let mut classes = HashSet::new();
 
         for class in ast.get_classes() {
+            classes.insert(class.get_name());
             for function in class.get_methods() {
                 contexts.push(FunctionContext::from(Some(class), function));
             }
         }
 
-        Ok(Functions(contexts))
+        Ok(Functions { classes, contexts })
     }
 }
